70f0e91bd39879bb4c2ad6b60280e8f28b1d49ae
k0wal45 <d.kowal.com@gmail.com>
2024-06-25 12:58:42 +0200

    why it isnt working

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
   return (
     <section className="flex justify-center items-end min-h-screen max-w-screen relative overflow-hidden border-b-2 border-neutral-200 pb-0 p-4 bg-neutral-200">
 			<div className="text-start flex flex-col lg:flex-row lg:pb-0 pt-32 lg:pt-0 justify-center items-center gap-24 px-8">
-				<div className="flex flex-col gap-4 max-w-md w-full p-4 ">
-					<h1 className={`${noto.className} font-bold text-5xl lg:text-6xl max-w-screen`}>Zadbaj o <p className='text-primary my-2'>swoją skórę</p> z Perfect Skin</h1>
+				<div className="flex flex-col gap-4 max-w-md w-full">
+					<h1 className={`${noto.className} font-bold text-5xl lg:text-6xl max-w-sm lg:max-w-md`}>Zadbaj o <p className='text-primary my-2'>swoją skórę</p> z Perfect Skin</h1>
 					<p className="text-xl" >Zadbaj o swoją skórę z pomocą naszych doświadczonych specjalistów. Oferujemy szeroką gamę zabiegów kosmetycznych dostosowanych do Twoich potrzeb. Zarezerwuj wizytę online i odkryj piękno swojej skóry.</p>
 					<BtnLink link="/kontakt">Zarezewuj wizytę</BtnLink> 
 				</div>
