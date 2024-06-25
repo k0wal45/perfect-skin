153eaa88fe863a5b892f1a315ed4770da94db5dc
k0wal45 <d.kowal.com@gmail.com>
2024-06-25 12:52:30 +0200

    change

--- app/(page)/onas/page.tsx
+++ app/(page)/onas/page.tsx
@@ -2,18 +2,20 @@ import About from '@/components/About/About'
 import Contact from '@/components/Contact/Contact'
 import Galery from '@/components/Galery/Galery'
 import Services from '@/components/Services/Services'
+import Stats from '@/components/Stats'
 import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
     <main className="overflow-x-hidden max-w-screen">
-      <section className="relative w-full py-32 flex items-center justify-start overflow-hidden bg-white/60">
-        <h1 className="w-full lg:w-1/2 grid place-items-center font-bold text-6xl text-black">O Nas</h1>
+      <section className="relative w-full py-32 flex items-center justify-start overflow-hidden bg-primary/60">
+        <h1 className="w-full lg:w-1/2 grid place-items-center font-bold text-6xl text-white">O Nas</h1>
         <Image width={1920} height={1080} src='/img/astetic.jpg' alt='Astetyczne Kosmetyki' className='absolute w-full object-cover object-center top-0 left-0 h-full z-[-1]' />
       </section>
       <About />
       <Galery />
+      <Stats />
       <Contact />
       <Services />
     </main>
---
+++ components/Experts/ExpertDescription.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExpertDescription = () => {
+  return (
+    <section className=""></section>
+  )
+}
+
+export default ExpertDescription
